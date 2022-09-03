636feef3be615970e91bd7fc6b597783bf880029
Wangzhijie1209 <2201792896@qq.com>
2022-09-03 21:38:04 +0800

    Toolbar的简单使用

---
+++ README.md
@@ -0,0 +1,12 @@
+Toolbar:不仅继承了ActionBar的所有功能,而且灵活性很高,可以配合其他控件来完成一些Material Design的效果
+使用:
+AndroidManifest.xml中的Application的属性 android:theme="" 属性指定了了一个AppTheme的主题,这个主题在:
+res/values/themes.xml中
+
+使用Toolbar来替代ActionBar,需要指定一个不带ActionBar的主题:通常有:
+Theme.AppCompat.NoActionBar 表示深色主题,它会将界面的主主体演示设成深色,陪衬颜色设成淡色
+Theme.AppCompat.Light.NoActionBar  表示淡色主题,它会将界面的主体颜色设成淡色,陪衬颜色设成深色,
+之前的程序都是以淡色为主的
+
+在AndroidManifest.xml中的Activity标签中增加label属性 用于指定在Toolbar中显示文字内容 如果没有指定
+的话,会默认使用application中指定的label内容,也就是我们的应用名称
--- app/src/main/java/com/example/materialdesign/MainActivity.java
+++ app/src/main/java/com/example/materialdesign/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.materialdesign;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    /**
+     * 加载了toolbar.xml这个菜单文件
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    /**
+     * 处理菜单中各个按钮的点击事件
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.backup:
+                Toast.makeText(this, "You clicked Backup", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.delete:
+                Toast.makeText(this, "You clicked Delete", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.settings:
+                Toast.makeText(this, "You clicked Settings", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
     }
 }
